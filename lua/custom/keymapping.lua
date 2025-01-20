398d089bdac43f720078bf64fde608b0638701c8
Jason Dong <jasondoong@gmail.com>
2025-01-20 18:31:34 +0800

    add test shourtcut

--- lua/custom/keymapping.lua
+++ lua/custom/keymapping.lua
@@ -2,4 +2,10 @@ local map = vim.keymap.set
 
 map('n', '<leader>cs', require('custom.show_class_methods').show_classes_and_methods, { desc = 'Open filtered doucment symbols in Quickfix list' })
 
+-- test shortcuts
+map('n', '<leader>tr', ':lua require("neotest").run.run()<CR>', { desc = 'run the nearest test' })
+map('n', '<leader>to', ':lua require("neotest").output.open({ enter = true })<CR>', { desc = 'open test output window' })
+map('n', '<leader>ts', ':lua require("neotest").summary.toggle()<CR>', { desc = 'open test summary window' })
+map('n', '<leader>tf', ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>', { desc = 'open test summary window' })
+map('n', '<leader>td', ':lua require("neotest").run.run({stratege = "dap"})<CR>', { desc = 'run dap on nearest test' })
 
