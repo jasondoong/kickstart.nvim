9e3a2258f5f7de8384f0b69949a7cc8636c13817
Jason Dong <jasondoong@gmail.com>
2025-02-11 18:15:25 +0800

    turn off auto-formatting

--- init.lua
+++ init.lua
@@ -755,7 +755,7 @@ require('lazy').setup({
 
   { -- Autoformat
     'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
+    -- event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
     keys = {
       {
