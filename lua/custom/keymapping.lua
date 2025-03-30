f5d333d2998654d8321aee1622c41dc60a5abe4a
Jason Dong <jasondoong@gmail.com>
2025-03-30 23:58:47 +0800

    use ToggleTerm for terminal and lazygit

--- lua/custom/keymapping.lua
+++ lua/custom/keymapping.lua
@@ -14,14 +14,23 @@ map('n', '<leader>to', ':lua require("neotest").output_panel.toggle()<CR>', { de
 map('n', '<leader>ts', ':lua require("neotest").summary.toggle()<CR>', { desc = 'open test summary window' })
 map('n', '<leader>tf', ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>', { desc = 'run tests in the file' })
 map('n', '<leader>td', ':lua require("neotest").run.run({stratege = "dap"})<CR>', { desc = 'run dap on nearest test' })
-
 -- debug shortcuts
 map('n', '<leader>dr', ':lua require("dap-python").test_method()<CR>', { desc = 'Debug: run nearest.' })
 
 -- terminal
-map('n', '<leader>h', ':horizontal terminal<CR>', { desc = 'split horizontal terminal' })
-map('n', '<leader>v', ':vertical terminal<CR>', { desc = 'split vertical terminal' })
+-- map('n', '<leader>h', ':horizontal terminal<CR>', { desc = 'split horizontal terminal' })
+-- map('n', '<leader>v', ':vertical terminal<CR>', { desc = 'split vertical terminal' })
+map('n', '<leader>z', ':ToggleTerm name=default<CR>', { desc = 'toggle default terminal' })
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+map("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { desc = 'toggle lazygit' })
 
 -- quickfix
 map('n', '<leader>co', ':copen<CR>', { desc = 'open quickfix window' })
-map('n', '<leader>cc', ':cclose<CR>', { desc = 'close quickfix window' })
+map('n', '<leader>cc', ':cclose<CR>', { desc = 'close quico/fix window' })
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,4 +6,13 @@ return {
   { 'sitiom/nvim-numbertoggle' },
   { 'github/copilot.vim' },
   { 'jessekelighine/vindent.vim' },
+  { 'akinsho/toggleterm.nvim', version = "*",
+    opts = {
+      direction = 'float',
+      float_opts = {
+        width = math.floor(vim.o.columns * 0.9),
+        height = math.floor(vim.o.lines * 0.9),
+      }
+    },
+  }
 }
