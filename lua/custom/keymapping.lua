e6044de0147b08812a11da2ccb69981e786ef411
Jason Dong <jasondoong@gmail.com>
2025-06-09 23:11:23 +0800

    use kitty to run pytest --lf

--- lua/custom/keymapping.lua
+++ lua/custom/keymapping.lua
@@ -1,8 +1,23 @@
 local map = vim.keymap.set
 
+local kitty_listen_on = os.getenv('KITTY_LISTEN_ON')
+
 -- test shortcuts
 map('n', '<leader>ta', ':Neotest attach<CR>', { desc = 'Neotest attach' })
-map('n', '<leader>tl', ':lua require("neotest").run.run_last()<CR>', { desc = 'run the last test' })
+-- map('n', '<leader>tl', ':lua require("neotest").run.run_last()<CR>', { desc = 'run the last test' })
+-- ~/.config/nvim/init.lua or other keymap file
+vim.keymap.set('n', '<leader>tl', function()
+  if kitty_listen_on then
+    local cmd = string.format(
+      'silent !kitty @ --to %s send-text --match var:id=main_bottom "pytest --lf\\n"',
+      kitty_listen_on
+    )
+    -- print(cmd)
+    vim.cmd(cmd)
+  else
+    print("Kitty is not running or KITTY_LISTEN_ON is not set.")
+  end
+end, { desc = '[T]est [L]ast in runner pane' })
 -- map('n', '<leader>tt', ':lua require("neotest").run.run()<CR>', { desc = 'run the nearest test' })
 map('n', '<leader>tt', function()
   require("neotest").run.run()
@@ -21,6 +36,7 @@ map('n', '<leader>dr', ':lua require("dap-python").test_method()<CR>', { desc = 
 -- map('n', '<leader>h', ':horizontal terminal<CR>', { desc = 'split horizontal terminal' })
 -- map('n', '<leader>v', ':vertical terminal<CR>', { desc = 'split vertical terminal' })
 map('n', '<leader>z', ':ToggleTerm name=default<CR>', { desc = 'toggle default terminal' })
+map('t', 'zz', [[<C-\><C-n>]], { desc = 'to normal mode' })
 
 local Terminal  = require('toggleterm.terminal').Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
