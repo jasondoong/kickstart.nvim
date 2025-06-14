2b68607d20a872db691f1bc67d4cd065c6e482ad
Jason Dong <jasondoong@gmail.com>
2025-06-15 00:57:45 +0800

    config for indent folding

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
+-- 使用縮排作為摺疊方式
+vim.opt.foldmethod = "indent"
+-- 預設開啟所有摺疊，或設定為其他數字來控制預設摺疊層級
+vim.opt.foldlevel = 99
+-- 摺疊欄位，顯示摺疊符號
+vim.opt.foldcolumn = "1"
+
 if vim.g.vscode then
   vim.keymap.set("i", "jk", "<Esc>", { desc = "jk to escape" })
 else
