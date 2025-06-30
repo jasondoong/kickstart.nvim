4a1851ee7886df76dff50011b4c6243d4b08327c
Jason Dong <jasondoong@gmail.com>
2025-06-30 21:52:18 +0800

    Use treesitter folding for Python

--- init.lua
+++ init.lua
@@ -5,6 +5,15 @@ vim.opt.foldlevel = 99
 -- 摺疊欄位，顯示摺疊符號
 vim.opt.foldcolumn = "1"
 
+-- Use Tree-sitter folding for Python files, focusing on class and function definitions
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'python',
+  callback = function()
+    vim.opt_local.foldmethod = 'expr'
+    vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
+  end,
+})
+
 if vim.g.vscode then
   vim.keymap.set("i", "jk", "<Esc>", { desc = "jk to escape" })
 else
