ac5372959091f0869da2295be35ffe78cfecdede
Jason Dong <jasondoong@gmail.com>
2025-07-04 00:00:05 +0800

    remove unused configs

--- init.lua
+++ init.lua
@@ -18,8 +18,4 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
-if vim.g.vscode then
-  vim.keymap.set("i", "jk", "<Esc>", { desc = "jk to escape" })
-else
-  require 'my_kickstart'
-end
+require 'my_kickstart'
